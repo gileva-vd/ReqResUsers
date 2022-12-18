a83e2588473ac46c6831b9d62b99c77e9c8f60f2
Valeriya Gileva <62500965+gileva-vd@users.noreply.github.com>
2022-12-18 10:58:52 +0300

    Initial Commit

---
+++ ReqResUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReqResUsers
+//
+//  Created by Валерия Гилева on 18.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
